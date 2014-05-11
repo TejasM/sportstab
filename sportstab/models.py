30b45b8c76d50a2bbd82f57f5f83f8336833d4c4
Tejas <tejasmehta0@gmail.com>
2014-05-11 11:41:09 -0400

    Add models

--- sportstab/models.py
+++ sportstab/models.py
@@ -3,6 +3,16 @@ from django.db import models
 
 
 # Create your models here.
+class Team(models.Model):
+    team_name = models.CharField(max_length=400)
+    users = models.ManyToManyField(User)
+
+
+class Play(models.Model):
+    name = models.CharField(max_length=400)
+    accessible_by = models.ManyToManyField(User)
+
+
 class VideoPlay(models.Model):
     video = models.FileField(upload_to='video/', null=True)
-    accessible_by = models.ManyToManyField(User)
+    play = models.ForeignKey(Play)
