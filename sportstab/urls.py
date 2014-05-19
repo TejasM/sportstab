7391d1cebac75af86f5a982a87511d0402c8afbd
stefanhadjis <stefanhadjis@gmail.com>
2014-05-19 12:34:56 -0400

    Add url for create play

--- sportstab/urls.py
+++ sportstab/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
                        # Examples:
                        url(r'^view_play/(?P<play_id>\d+)$', views.view_play, name='view_play'),
                        url(r'^create_team/$', views.create_team, name='create_team'),
+                       url(r'^create_play$', views.create_play),
                        url(r'^view_team/(?P<team_id>\d+)$', views.view_team, name='view_team'),
 )
