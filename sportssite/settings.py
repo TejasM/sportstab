22f10c074f575637528a6e6064a5b34483eaa4eb
Tejas <tejasmehta0@gmail.com>
2014-05-26 16:58:06 -0400

    External database

--- sportssite/settings.py
+++ sportssite/settings.py
@@ -126,6 +126,18 @@ TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), '..', 'templates').repl
 # # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sportstab',  # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'tejas',
+        'PASSWORD': 'tejas',
+        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',  # Set to empty string for default.
+    }
+}
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
